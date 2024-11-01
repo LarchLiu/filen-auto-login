82a144dce33d3e24ff306a883cd3f05660b25271
Alex.Liu <larch.liu@gmail.com>
2024-11-01 22:45:42 +0800

    fix: serverless function

--- api/login.js
+++ api/login.js
@@ -1,43 +1,52 @@
-import { waitUntil } from '@vercel/functions';
- 
+import fetch from 'node-fetch';
+
 const CRON_SECRET = process.env.CRON_SECRET;
-export function GET(req) {
-  const env = getEnv();
-  const CRON_SECRET = env.CRON_SECRET;
+
+export default async function handler(req, res) {
   const authHeader = req.headers['authorization'];
 
   if (authHeader && authHeader.startsWith('Bearer ')) {
     if (authHeader.substring(7) === CRON_SECRET) {
-      const accounts = [];
-      let index = 1;
-      while (env[`HOST${index}`]) {
-        accounts.push({
-          email: env[`EMAIL${index}`],
-          password: env[`PASSWORD${index}`],
-        });
-        index++;
-      }
-
-      // 处理每个账户
-      let result = 'success';
-      for (const account of accounts) {
-        waitUntil(fetch(`https://gateway.filen-3.net/v3/login`, {
-          method: 'POST',
-          body: JSON.stringify({
-            email: account.email,
-            password: account.password,
-            twoFactorCode: 'XXXXXX',
-            authVersion: 2
-          })
-        }));
-      }
-
-      return new Response(result);
+      return handleRequest(req, res);
     } else {
-      return new Response('Unauthorized', {
-        status: 401
-      });
+      return res.status(401).send('Unauthorized');
     }
   }
+
+  return res.status(401).send('Authentication required');
 }
 
+async function handleRequest(req, res) {
+  const accounts = [];
+  let index = 1;
+  while (process.env[`HOST${index}`]) {
+    accounts.push({
+      email: process.env[`EMAIL${index}`],
+      password: process.env[`PASSWORD${index}`],
+      twoFactorCode: 'XXXXXX',
+      authVersion: 2
+    });
+    index++;
+  }
+
+  // 处理每个账户
+  let result = 'success';
+  for (const account of accounts) {
+    try {
+      await fetch(`https://gateway.filen-3.net/v3/login`, {
+        method: 'POST',
+        body: JSON.stringify({
+          email: account.email,
+          password: account.password,
+          twoFactorCode: 'XXXXXX',
+          authVersion: 2
+        })
+      });
+    } catch (error) {
+      result = 'failed';
+      break;
+    }
+  }
+
+  res.status(200).send(result);
+}
